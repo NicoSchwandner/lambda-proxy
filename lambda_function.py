bbc565c59911be06207918c42aae55c6a69757e7
Nicolas Schwandner <nico.schwandner@gmail.com>
2024-12-08 20:42:10 +0100

    Parse incoming Slack request and handle form-urlencoded data

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,6 @@
 import boto3
 import json
+from urllib.parse import parse_qs
 from config import LUNCH_MENU_FETCHER_NAME
 
 def determine_lambda_name(path_segments: list[str]) -> str:
@@ -20,8 +21,18 @@ def call_secondary_lambda(function_name: str, payload: dict):
     )
 
 def lambda_handler(event, context):
+    # Parse the incoming Slack request
+    if event.get('isBase64Encoded'):
+        import base64
+        body_str = base64.b64decode(event['body']).decode('utf-8')
+    else:
+        body_str = event.get('body', '')
+
+    # Slack sends data as form-urlencoded; parse it
+    parsed_body = parse_qs(body_str)
+
     proxy_path = event.get('pathParameters', {}).get('proxy', '')
-    response_url = event.get('response_url')
+    response_url = parsed_body.get('response_url', [None])[0]
 
     # Split the path into segments
     path_segments = proxy_path.split('/')
